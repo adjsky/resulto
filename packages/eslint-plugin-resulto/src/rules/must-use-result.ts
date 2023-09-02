fc9ac8d588a5b6176ff7834d73ad1b9fd35e87fe
adjsky <igorlfmartins@mail.ru>
2023-09-02 21:43:12 +0300

    fix rule doc path

--- packages/eslint-plugin-resulto/src/rules/must-use-result.ts
+++ packages/eslint-plugin-resulto/src/rules/must-use-result.ts
@@ -8,7 +8,7 @@ import type { TypeChecker, TypeReference } from "typescript"
 
 const ruleCreator = ESLintUtils.RuleCreator(
   (name) =>
-    `https://github.com/adjsky/resulto/tree/master/packages/eslint-plugin-resulto/docs/rules/${name}`
+    `https://github.com/adjsky/resulto/tree/master/packages/eslint-plugin-resulto/docs/rules/${name}.md`
 )
 
 const rule = ruleCreator({
