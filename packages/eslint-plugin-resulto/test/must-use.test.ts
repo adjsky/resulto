8b83c5a5567c1f136f09f7c780b7619d9bea260f
Kirill Timchenko <49305219+adjsky@users.noreply.github.com>
2023-09-09 13:23:58 +0300

    Fix plugin (#18)
    Add test cases
    
    Add additional checks
    
    Add hint to README.md

--- packages/eslint-plugin-resulto/test/must-use.test.ts
+++ packages/eslint-plugin-resulto/test/must-use.test.ts
@@ -17,10 +17,10 @@ tester.run("must-use-result", rule, {
       code: injectCode(`
         const okResult = ok(5)
         const errResult = err(5)
-    
+
         ok(5).match(noop, noop)
         ok(5).asyncMatch(noop, noop)
-    
+
         err(5).match(noop, noop)
         err(5).asyncMatch(noop, noop)
       `)
@@ -36,10 +36,10 @@ tester.run("must-use-result", rule, {
       code: injectCode(`
         const okAsyncResult = okAsync(5)
         const errAsyncResult = errAsync(5)
-    
+
         okAsync(5).match(noop, noop)
         okAsync(5).asyncMatch(noop, noop)
-    
+
         errAsync(5).match(noop, noop)
         errAsync(5).asyncMatch(noop, noop)
       `)
@@ -49,10 +49,10 @@ tester.run("must-use-result", rule, {
       code: injectCode(`
         const okAsyncResult = await okAsync(5)
         const errAsyncResult = await errAsync(5)
-    
+
         (await okAsync(5)).match(noop, noop)
         (await okAsync(5)).asyncMatch(noop, noop)
-    
+
         (await errAsync(5)).match(noop, noop)
         (await errAsync(5)).asyncMatch(noop, noop)
       `)
@@ -119,9 +119,93 @@ tester.run("must-use-result", rule, {
         function doSomething(res: Result<unknown, unknown>) {
           //
         }
-
         doSomething(ok(4))
         doSomething(err(5))
+      `)
+    },
+
+    {
+      code: injectCode(`
+        const _ = [ok(1), err(2), okAsync(3), errAsync(4)]
+      `)
+    },
+    {
+      code: injectCode(`
+        [ok(3), err(5), okAsync(2), errAsync(0)]
+      `)
+    },
+
+    {
+      code: injectCode(`
+        const _ = {
+          res: ok(2)
+        }
+      `)
+    },
+
+    {
+      code: injectCode(`
+        () => ok(1)
+      `)
+    },
+
+    {
+      code: injectCode(`
+        function fn() {
+          return ok(1)
+        }
+      `)
+    },
+
+    {
+      code: injectCode(`
+        let res
+        res = ok(1)
+      `)
+    },
+
+    {
+      code: injectCode(`
+        let someBool = false
+        const _ = someBool ? ok(4) : err(2)
+        const _ = someBool && ok(4)
+        const _ = someBool || err(4)
+        const _ = someBool ?? ok(2)
+      `)
+    },
+    {
+      code: injectCode(`
+        let res
+        res = false ? ok(4) : err(2)
+        res = true && ok(4)
+        res = false || err(4)
+        res = true ?? ok(2)
+      `)
+    },
+
+    {
+      code: injectCode(`
+        function fn(res: Result<unknown, unknown>) {
+          //
+        }
+
+        fn(false && ok(4))
+        fn(false ? ok(4) : err(5))
+      `)
+    },
+
+    {
+      code: injectCode(`
+        function test() {
+          return ok(4).chain()
+        }
+      `)
+    },
+    {
+      code: injectCode(`
+        async function test() {
+          return await okAsync(4).chain()
+        }
       `)
     }
   ],
@@ -133,6 +217,7 @@ tester.run("must-use-result", rule, {
       `),
       errors: [{ messageId: "mustUse" }, { messageId: "mustUse" }]
     },
+
     {
       code: injectCode(`
         new Ok(5)
