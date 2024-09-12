a7ceb0f9d48ca07396bb2e897a1651b715e428d9
Kirill Timchenko <49305219+adjsky@users.noreply.github.com>
2024-09-12 18:46:21 +0300

    bump eslint (#24)
    * bump eslint
    
    * sync lockfile

--- packages/eslint-plugin-resulto/src/index.ts
+++ packages/eslint-plugin-resulto/src/index.ts
@@ -1,11 +1,18 @@
+import packageJson from "../package.json"
 import recommended from "./configs/recommended"
 import mustUseResult from "./rules/must-use-result"
+
+import type { Linter } from "@typescript-eslint/utils/ts-eslint"
 
 export default {
   configs: {
     recommended
   },
+  meta: {
+    name: packageJson.name,
+    version: packageJson.version
+  },
   rules: {
     "must-use-result": mustUseResult
   }
-}
+} satisfies Linter.Plugin
