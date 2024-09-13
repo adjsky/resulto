5666f6d7d339ccc89b659c48dee1b9fb43f983a6
adjsky <igorlfmartins@mail.ru>
2024-09-13 09:17:38 +0300

    fix eslint

--- packages/eslint-plugin-resulto/src/configs/recommended.ts
+++
@@ -1,8 +0,0 @@
-import type { ClassicConfig } from "@typescript-eslint/utils/ts-eslint"
-
-export default {
-  plugins: ["resulto"],
-  rules: {
-    "resulto/must-use-result": "error"
-  }
-} satisfies ClassicConfig.Config
