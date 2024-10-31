621e642fc503a1054e109c67d90f0e0411ebd8d6
adjsky <igorlfmartins@mail.ru>
2024-10-31 21:11:50 +0300

    format

--- publish_npm.ts
+++ publish_npm.ts
@@ -2,6 +2,6 @@ import denoJson from "./deno.json" with { type: "json" };
 import { $ } from "@david/dax";
 
 for (const cwd of denoJson.workspace) {
-    await $`cd ${cwd} && deno run -A ./build_npm.ts`;
-    await $`npm publish ${cwd}/npm`;
+	await $`cd ${cwd} && deno run -A ./build_npm.ts`;
+	await $`npm publish ${cwd}/npm`;
 }
