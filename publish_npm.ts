ec56fcb50b96c79a208b36aa52375428e57b780d
adjsky <igorlfmartins@mail.ru>
2024-10-31 21:09:38 +0300

    ci: add publish action

---
+++ publish_npm.ts
@@ -0,0 +1,7 @@
+import denoJson from "./deno.json" with { type: "json" };
+import { $ } from "@david/dax";
+
+for (const cwd of denoJson.workspace) {
+    await $`cd ${cwd} && deno run -A ./build_npm.ts`;
+    await $`npm publish ${cwd}/npm`;
+}
