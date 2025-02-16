a39fb5a815e82f17380946c4d8d8f009f19e0ec7
adjsky <igorlfmartins@mail.ru>
2025-02-16 18:56:23 +0300

    fix: syntax?

--- publish_npm.ts
+++ publish_npm.ts
@@ -3,5 +3,5 @@ import { $ } from "@david/dax";
 
 for (const cwd of denoJson.workspace) {
   await $`cd ${cwd} && deno run -A ./build_npm.ts`;
-  await $`cd ${cwd}/npm && npm publish || :`;
+  await $`cd ${cwd}/npm && (npm publish || true)`;
 }
